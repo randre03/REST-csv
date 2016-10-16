c2fd14851b8c904abf98e58680bdd01a1b701a01
Roger Randall <roger@rogerrandall.com>
2016-10-16 15:51:12 -0400

    Partial implementation of data model.

--- src/rest_csv/core.clj
+++ src/rest_csv/core.clj
@@ -7,15 +7,40 @@
 
 (hugsql/def-db-fns "rest_csv/database.sql")
 
+(defn init-db []
+  (install-uuid-module db)
+  (create-endpoint-table db)
+  (create-upload-table db)
+  (create-row-table db)
+  (create-cell-table db))
+
+(defn reset-db! []
+  (init-db)
+  (delete-all-cells db)
+  (delete-all-rows db)
+  (delete-all-uploads db)
+  (delete-all-endpoints db))
+
 (defn create-endpoint [db]
   (:id (create-endpoint* db)))
+
+(defn create-upload [db endpoint-id]
+  (:id (create-upload* db {:endpoint-id endpoint-id})))
+
+(defn create-row [db upload-id]
+  (:id (create-row* db {:upload-id upload-id})))
+
+(defn create-cell [db row-id key value]
+  (:id (create-cell* db {:row-id row-id
+                         :key key
+                         :value value})))
 
 (defn app [req]
   {:headers {}
    :status 200
    :body "Hello World"})
 
-
 (defn -main []
+  (init-db)
   (jetty/run-jetty app
                    {:port 8080}))
